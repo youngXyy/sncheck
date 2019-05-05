e8c531d8f8de749f7d2fecc771baea4d09ad25c9
young <847543216@qq.com>
2019-05-05 20:56:26 +0800

    sncheck

---
+++ src/main/java/com/ruijie/sncheck/SncheckApplication.java
@@ -0,0 +1,12 @@
+package com.ruijie.sncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SncheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SncheckApplication.class, args);
+    }
+
+}
