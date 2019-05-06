bfc8c9aeb68048119ea9251c92e59ebaf125caa0
wuyanCoder <1352391749@qq.com>
2019-05-06 13:18:18 +0800

    初始化项目

---
+++ src/main/java/com/ruijie/sncheck/SncheckApplication.java
@@ -0,0 +1,13 @@
+package com.ruijie.sncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SncheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SncheckApplication.class, args);
+    }
+
+}
